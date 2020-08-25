7d407041bf28d6c504cdae3d0746dbe1685c53de
AHMED AFANDY <eng.computer1@yahoo.com>
2020-08-25 15:44:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// Write your code here!
-
-var element = document.querySelector("main")
-element.remove(element)
-
-
-var newHeader =document.createElement("H1")
-newHeader.setAttribute("id","victory")
-newHeader.innerHTML='YOUR-NAME is the champion'
+var ahmed = document.createElement('h1')
+ahmed.innerHTML="Ahmet up"
+document.body.appedChild('ahmed')
